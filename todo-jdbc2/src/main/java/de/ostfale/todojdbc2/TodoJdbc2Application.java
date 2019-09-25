4edbe1ba782323af7bce41de4d1da8b32764f395
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2019-09-25 17:08:39 +0200

    Data - JDBC 2 Example

---
+++ todo-jdbc2/src/main/java/de/ostfale/todojdbc2/TodoJdbc2Application.java
@@ -0,0 +1,13 @@
+package de.ostfale.todojdbc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoJdbc2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoJdbc2Application.class, args);
+    }
+
+}
